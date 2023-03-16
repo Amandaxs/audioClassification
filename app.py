ab450e5527a11a78091fea749f9db3e8ce0b4f4b
Amandaxs <39634209+Amandaxs@users.noreply.github.com>
2023-03-16 15:01:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("""
+# Classificação de ruídos
+
+**Este App é destinado a demonstração do modelo de classificação de ruido**
+
+* O upload do arquivo deverá ser feito na aba "Upload"
+* Após a carga na aba, o audio ja fica disponível na aba "Classifyer"
+* Escolha o audio no seletor e os gráficos e resultados da página serão ajustados para o audio selecionado.
+
+""")
+
